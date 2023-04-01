2dafeaf90b2403cc2cedfb3476139e7d82154b9a
grazily <grazielydiasxx@gmail.com>
2023-03-31 22:14:19 -0400

    primeira alteração

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,196 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; //pacote flutter, bibliotecas do material app
+
+//Graziely Coelho Dias - Desenvolvimento para Dispositivos Móveis 5º período
 
 void main() {
-  runApp(const MyApp());
+  runApp(MeuAplicativo());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class MeuAplicativo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Construção dos Widget
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          primarySwatch: Colors.red,
+          textTheme: const TextTheme(
+              bodyMedium:
+                  TextStyle(fontFamily: "Arial", fontSize: 30))), // tema do app
+      home: Scaffold(
+        // Conjunto de funçôes
+        appBar: AppBar(
+          //Título do app
+          title: const Text("Meu Primeiro Aplicativo"),
+          centerTitle: true,
+        ),
+        body: Column(
+          //Column formatação em colunas
+          children: [
+            const Image(
+              width: 800, //proporção da imagem
+              // height: 500,
+              image: AssetImage('assets/flamengo01.jpg'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Container(
+              margin: const EdgeInsets.fromLTRB(
+                  15.0, 5.0, 20.0, 0), ////Posicionamneto
+              child: Row(
+                // Formatação em linhas
+                children: [
+                  const Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Flamengo", style: TextStyle(fontSize: 22)),
+                        Text("Clube de Regatas do Flamengo",
+                            style: TextStyle(
+                                //FontStyle.italic - Fonte em Italico
+                                fontSize: 15,
+                                fontStyle: FontStyle.italic)),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          child: const Column(
+                            children: [
+                              Text("Partidas",
+                                  style: TextStyle(color: Colors.black)),
+                              Icon(Icons.calendar_month,
+                                  color: Colors.blue), //Ícones
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: Row(
+                children: [
+                  const SizedBox(height: 100),
+                  //Botões de navegação
+                  TextButton(
+                    onPressed: () {},
+                    child: const Column(
+                      children: [
+                        Icon(
+                          Icons.auto_stories,
+                          color: Colors.blue,
+                        ),
+                        Text("História",
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 20)),
+                      ],
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Column(
+                      children: [
+                        Icon(
+                          Icons.workspace_premium_outlined,
+                          color: Colors.blue,
+                        ),
+                        Text("Títulos",
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 20)),
+                      ],
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Column(
+                      children: [
+                        Icon(Icons.signal_cellular_alt_outlined,
+                            color: Colors.blue),
+                        Text("Classificação",
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 20)),
+                      ],
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Column(
+                      children: [
+                        Icon(Icons.group, color: Colors.blue),
+                        Text("Formação",
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 20)),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+                margin: const EdgeInsets.fromLTRB(
+                    25.0, 5.0, 30.0, 30), //Posicionamneto do objeto
+                child: const Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        "Confira as principais notícias sobre o Flamengo no DCI. Acompanhe todas as novidades ligadas ao clube de futebol carioca e seus resultados. Matheus França e Gonçalves, do Flamengo, viram figurinhas digitais e oferecem experiências, entenda o NFT Dupla de joias do Flamengo têm suas imagens digitalizadas, e torcedores acumulam pontos de acordo com a performance deles em campo; resultados geram prêmios ",
+
+                        style: TextStyle(height: 1.5, fontSize: 16),
+                        textAlign: TextAlign
+                            .justify, //Formatação do objeto: height- espaçamento, fontSize- tamanho fonte, textAlign.justify - justificar texto
+                      ),
+                      //  n/ para quebrar linha
+                    ),
+                  ],
+                )),
+            Container(
+              margin: EdgeInsets.fromLTRB(90.0, 15.0, 10.0, 0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Column(
+                      children: [
+                        Text("Contato",
+                            style: TextStyle(color: Colors.blue, fontSize: 15)),
+                      ],
+                    ),
+                  ),
+
+                  //Botões de navegação rodapé
+                  TextButton(
+                    onPressed: () {},
+                    child: const Column(
+                      children: [
+                        Text("Ajuda",
+                            style: TextStyle(color: Colors.blue, fontSize: 15)),
+                      ],
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Column(
+                      children: [
+                        Text("Sugestões",
+                            style: TextStyle(color: Colors.blue, fontSize: 15)),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+//Desenvolvimento para Dispositivos Móveis 5º período
+//Atividade 2
+//Graziely
